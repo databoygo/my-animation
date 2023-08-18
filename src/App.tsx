02353f1bcdda33ee7cc14d3a972d3e2e71237587
dongrc <1321864816@qq.com>
2023-08-19 00:41:20 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Menu } from 'antd';
 import CssAnimation from './CssAnimation/index'
@@ -23,18 +23,18 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-  getItem('Navigation One', 'sub1', '<MailOutlined />', [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
     getItem('Option 1', '1'),
     getItem('Option 2', '2'),
     getItem('Option 3', '3'),
     getItem('Option 4', '4'),
   ]),
-  getItem('Navigation Two', 'sub2',' <AppstoreOutlined />', [
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
     getItem('Option 5', '5'),
     getItem('Option 6', '6'),
     getItem('Submenu', 'sub3', null, [getItem('Option 7', '7'), getItem('Option 8', '8')]),
   ]),
-  getItem('Navigation Three', 'sub4', '<SettingOutlined />', [
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
     getItem('Option 9', '9'),
     getItem('Option 10', '10'),
     getItem('Option 11', '11'),
