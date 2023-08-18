961c7a7312dbfced9526f94694ddf2ec068eb927
dongrc <1321864816@qq.com>
2023-08-18 23:27:26 +0800

    引入react框架

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CssAnimation from './pages/CssAnimation'
+function App() {
+  return (
+    <CssAnimation></CssAnimation>
+  );
+}
+
+export default App;
---
+++ src/pages/CssAnimation/index.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useRef, useState } from 'react';
+import './index.less';
+
+export interface IAppProps {
+}
+
+export default function App (props: IAppProps) {
+  const [width, setWidth] = useState('200px')
+
+
+  return (
+    <div className="container">
+      <div className="box" style={{width: width}}>鼠标移入变换，通过css修改</div>
+      <div className="handle">
+        <h1>通过js修改</h1>
+        <button className="button" onClick={()=>setWidth('400px')}>变为400</button>
+        <button className="button" onClick={()=>setWidth('200px')}>变为200</button>
+      </div>
+    </div>
+  );
+}
